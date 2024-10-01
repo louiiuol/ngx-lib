f58f0422e07a7dd2b348630888411089689c71c1
louiiuol <louis.godlewski@vidmizer.com>
2024-10-01 23:20:31 +0200

    [Format] Prettier formatting

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ngx-lib');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, ngx-lib',
+    );
   });
 });
