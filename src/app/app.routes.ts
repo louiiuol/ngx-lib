f4737f9b56f481e67d14d8fe42632535d9796bf1
louiiuol <louis.godlewski@vidmizer.com>
2024-10-06 14:30:14 +0200

    [Format] Prettier formatting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import type { Routes } from '@angular/router';
 
+/**
+ * Application routes
+ */
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/home.page').then((m) => m.HomePage),
+    loadComponent: () =>
+      import('./pages/home/home.page').then((m) => m.HomePage),
   },
 ];
