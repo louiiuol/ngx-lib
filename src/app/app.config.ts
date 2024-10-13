63db365717746cacb10cc687f7fa3cbc01495dbc
louiiuol <louis.godlewski@vidmizer.com>
2024-10-13 13:14:17 +0200

    [Core] Provide httpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import type { ApplicationConfig } from '@angular/core';
 import { provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(withFetch()),
   ],
 };
