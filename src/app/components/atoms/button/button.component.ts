94e1d7d3b22c6b72b61eb5978ac159fc2d214432
louiiuol <louis.godlewski@vidmizer.com>
2024-10-13 21:57:24 +0200

    [Component] Improving button styles and giving more controls

--- src/app/components/atoms/button/button.component.ts
+++ src/app/components/atoms/button/button.component.ts
@@ -1,18 +1,22 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 /**
- * Button component with different colors, sizes and rounded corners.
+ * Simple button component with multiple styles.
  *
  * @author louiiuol
  */
 @Component({
-  selector: 'lib-button, lib-button-outline',
+  selector: 'lib-button',
   standalone: true,
   host: {
+    class:
+      'inline-flex justify-center items-center rounded-md cursor-pointer overflow-hidden relative border-transparent border',
     '[attr.data-color]': 'color()',
     '[attr.data-size]': 'size()',
     '[attr.data-rounded]': 'rounded()',
     '[attr.data-disabled]': 'disabled()',
+    '[attr.data-appearance]': 'appearance()',
+    '[attr.data-full]': 'full()',
   },
   template: `<ng-content />`,
   styleUrls: ['./button.component.scss'],
@@ -21,21 +25,38 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 export class ButtonComponent {
   /**
    * Color of the button.
+   * @default 'primary'
    */
   color = input<'primary' | 'accent' | 'warn'>('primary');
 
   /**
    * Size of the button.
+   * @default 'medium'
    */
   size = input<'small' | 'medium' | 'large'>('medium');
 
   /**
-   * Whether the button should have rounded corners.
+   * Whether the button should take the full width.
+   * @default false
    */
-  rounded = input(false);
+  full = input(false);
+
+  /**
+   * Whether the button should have rounded corners.
+   * @default false
+   */
+  rounded = input(true);
 
   /**
    * Whether the button should be disabled.
+   * @default false
    */
   disabled = input(false);
+  // TODO Prevent click event when disabled
+
+  /**
+   * Appearance of the button.
+   * @default 'raised'
+   */
+  appearance = input<'basic' | 'flat' | 'stroked'>('flat');
 }
