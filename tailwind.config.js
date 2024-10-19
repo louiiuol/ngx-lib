43603444c7015477869af1c8e0c419bd1ffa8cc5
louiiuol <louis.godlewski@vidmizer.com>
2024-10-19 19:52:32 +0200

    [Config] Add tailwind default color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import { fontFamily as _fontFamily } from 'tailwindcss/defaultTheme';
 
 const primaryColor = {
+  DEFAULT: '#fb723c',
   '50': '#fff4ed',
   '100': '#ffe6d5',
   '200': '#febe99',
@@ -15,6 +16,7 @@ const primaryColor = {
 };
 
 const accentColor = {
+  DEFAULT: '#ed7f8d',
   '50': '#fef2f3',
   '100': '#fce7e8',
   '200': '#f9d2d6',
@@ -28,12 +30,28 @@ const accentColor = {
   '950': '#450c1b',
 };
 
+const punch = {
+  DEFAULT: '#ff7268',
+  '50': '#fff2f1',
+  '100': '#ffe2e0',
+  '200': '#ffcac6',
+  '300': '#ffa69f',
+  '400': '#ff7268',
+  '500': '#fb4538',
+  '600': '#e32416',
+  '700': '#c41d11',
+  '800': '#a21c12',
+  '900': '#861e16',
+  '950': '#490b06',
+};
+
 export const content = ["./src/**/*.{html,ts}"];
 export const theme = {
   extend: {
     colors: {
       primary: primaryColor,
       accent: accentColor,
+      warn: punch
     },
     fontFamily: {
       'sans': ['"Poppins"', ..._fontFamily.sans],
