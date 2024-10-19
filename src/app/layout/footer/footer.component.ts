b0492fcfd807a241a13fc758b10e843623fa6b37
louiiuol <louis.godlewski@vidmizer.com>
2024-10-19 19:52:01 +0200

    [UI] Improved footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,21 +1,34 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ButtonComponent } from 'src/app/components/atoms/button/button.component';
 
 /**
  * Simple footer component to display copyright
  *
+ * @internal
  * @author louiiuol
  */
 @Component({
   selector: 'lib-footer',
   host: {
     class:
-      'w-full flex  items-center justify-center bg-white text-blue-800 px-12 py-8',
+      'w-full flex  items-center justify-start bg-white text-blue-800 px-12 py-8',
   },
   standalone: true,
-  template: ` <p>{{ copyright }}</p> `,
-  imports: [],
+  template: `
+    <p>
+      Made with ❤️ by
+      <a
+        lib-button
+        class="ml-1 text-primary"
+        href="https://github.com/louiiuol"
+        target="_blank"
+        appearance="basic"
+        rounded
+        >louiiuol</a
+      >
+    </p>
+  `,
+  imports: [ButtonComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent {
-  copyright = 'Made with ❤️ by louiiuol';
-}
+export class FooterComponent {}
