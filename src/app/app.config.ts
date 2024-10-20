d44bd5f12fce05ef63075644f77c4bb1e2d3a067
louiiuol <louis.godlewski@vidmizer.com>
2024-10-20 11:26:11 +0200

    [CI] Update config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,11 @@
 import type { ApplicationConfig } from '@angular/core';
 import { provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import {
+  provideRouter,
+  withHashLocation,
+  withInMemoryScrolling,
+} from '@angular/router';
 import {
   NG_DOC_DEFAULT_PAGE_PROCESSORS,
   NG_DOC_DEFAULT_PAGE_SKELETON,
@@ -36,6 +40,7 @@ export const appConfig: ApplicationConfig = {
         scrollPositionRestoration: 'enabled',
         anchorScrolling: 'enabled',
       }),
+      withHashLocation(),
     ),
     provideHttpClient(withInterceptorsFromDi(), withFetch()),
     provideNgDocContext(),
