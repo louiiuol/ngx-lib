8277952a698407ea093cf444b401ee81c36737b0
louiiuol <louis.godlewski@vidmizer.com>
2024-10-20 12:04:32 +0200

    [Refactoring] Cleaning method

--- src/app/services/file/file.service.ts
+++ src/app/services/file/file.service.ts
@@ -3,6 +3,15 @@ import { inject, Injectable } from '@angular/core';
 import { catchError, map, of } from 'rxjs';
 
 /**
+ * Simple fallback SVG content to display when the requested SVG file is not found.
+ */
+const fallbackSVG = `
+<svg width="24" height="24" viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg">
+  <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM13 7H11V13H13V7ZM13 15H11V17H13V15Z" />
+</svg>
+`;
+
+/**
  * Simple service to handle file operations.
  *
  * @author louiiuol
@@ -20,18 +29,8 @@ export class FileService {
     return this.http.get(path, { responseType: 'text' }).pipe(
       catchError(() => of(null)),
       map((svgContent) => {
-        if (!svgContent) {
-          const fallbackSVG = `
-            <svg width="24" height="24" viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg">
-              <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM13 7H11V13H13V7ZM13 15H11V17H13V15Z" />
-            </svg>
-          `;
-          const tempDiv = document.createElement('div');
-          tempDiv.innerHTML = fallbackSVG;
-          return tempDiv.querySelector('svg');
-        }
         const tempDiv = document.createElement('div');
-        tempDiv.innerHTML = svgContent;
+        tempDiv.innerHTML = svgContent ?? fallbackSVG;
         return tempDiv.querySelector('svg');
       }),
     );
